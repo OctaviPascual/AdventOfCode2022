2176de1d043b2c3e9e0bfda937f01136ca995326
Octavi Pascual <octavi94@gmail.com>
2024-05-19 20:40:19 +0200

    Add day15

---
+++ day15/day15_test.go
@@ -0,0 +1,35 @@
+package day15
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewDay(t *testing.T) {
+	expected := &Day{}
+	input := ``
+	actual, err := NewDay(input)
+	require.NoError(t, err)
+
+	assert.Equal(t, expected, actual)
+}
+
+func TestSolvePartOne(t *testing.T) {
+	day := &Day{}
+
+	answer, err := day.SolvePartOne()
+	require.NoError(t, err)
+
+	assert.Equal(t, "", answer)
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	day := &Day{}
+
+	answer, err := day.SolvePartTwo()
+	require.NoError(t, err)
+
+	assert.Equal(t, "", answer)
+}
