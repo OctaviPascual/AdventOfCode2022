8e4f24a5dfdeee8b067a623b005af797133186a5
Octavi Pascual <octavi94@gmail.com>
2025-08-24 12:29:21 +0200

    Add day 21

---
+++ day21/day21.go
@@ -0,0 +1,20 @@
+package day21
+
+// Day holds the data needed to solve part one and part two
+type Day struct {
+}
+
+// NewDay returns a new Day that solves part one and two for the given input
+func NewDay(input string) (*Day, error) {
+	return &Day{}, nil
+}
+
+// SolvePartOne solves part one
+func (d Day) SolvePartOne() (string, error) {
+	return "", nil
+}
+
+// SolvePartTwo solves part two
+func (d Day) SolvePartTwo() (string, error) {
+	return "", nil
+}
